36aa6d8e623c4cc00a78f2c1da038af3f58cf192
spectatorNan <spectatornan@gmail.com>
2017-05-12 21:00:58 +0800

    fix bug

--- String_Extension.swift
+++ String_Extension.swift
@@ -10,20 +10,32 @@ import Foundation
 
 extension String {
     
+    /// 获取某个位置的字符串
+    ///
+    /// - Parameter at: 位置
+    /// - Returns: 返回要获取的值
     func value(at: Int) -> String {
         let index = self.index(self.startIndex, offsetBy: at)
         let boolStr = self[index]
         return "\(boolStr)"
     }
     
+    
+    /// 截取两个位置之间的值
+    ///
+    /// - Parameters:
+    ///   - star: 开始位置
+    ///   - end: 结束位置
+    /// - Returns: 获取的字符串
     func substring(star: Int, end: Int) -> String {
-        let new = self[self.startIndex.advance(2, for: self)..<self.startIndex.advance(4, for: self)]
+        let new = self[self.startIndex.advance(star, for: self)..<self.startIndex.advance(end, for: self)]
         
         return new
     }
 }
 
 extension String.Index{
+    
     func successor(in string:String)->String.Index{
         return string.index(after: self)
     }
@@ -32,6 +44,7 @@ extension String.Index{
         return string.index(before: self)
     }
     
+    /// 获取字符串某个位置的index
     func advance(_ offset:Int, `for` string:String)->String.Index{
         return string.index(self, offsetBy: offset)
     }
